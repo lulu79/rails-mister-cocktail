ef90df202066c374b899bb5e29b25d38e077b046
Ludivine Robin <ludivine.robin@kedgebs.com>
2019-05-24 12:44:28 +0200

    doses added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,14 +1,64 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
-  end
-
-  def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
     @cocktail = Cocktail.new
   end
 
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    respond_to do |format|
+      if @cocktail.save
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
+        format.json { render :show, status: :created, location: @cocktail }
+      else
+        format.html { render :new }
+        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @ingredients = @cocktail.ingredients
+    @ingredient = Ingredient.new
+    @dose = Dose.new
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @cocktail.update(cocktail_params)
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully updated.' }
+        format.json { render :show, status: :ok, location: @cocktail }
+      else
+        format.html { render :edit }
+        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    respond_to do |format|
+      format.html { redirect_to cocktails_url, notice: 'Cocktail was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+      params.require(:cocktail).permit(:name)
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,23 +1,30 @@
 class DosesController < ApplicationController
-  def index
-    @doses = Dose.all
-  end
-
-  def show
-    @dose = Dose.find(params[:id])
-  end
 
   def new
-     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
-    @dose = Dose.new(name: params[:name], cocktail: params[:cocktail])
-    @dose.save
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
